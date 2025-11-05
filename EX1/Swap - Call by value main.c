3f2547798293f431d6c8da6b2e58dd735cb18d5e
williechen27 <williechen27@gmail.com>
2025-11-05 22:12:02 +0800

    Update Swap - Call by value main.c

--- EX1/Swap - Call by value main.c
+++ EX1/Swap - Call by value main.c
@@ -37,3 +37,22 @@ int main() {
     printf("in main, i = %d, j = %d\n", i, j);
     return 0;
 }
+
+/* Swap function with pointer */
+#include <stdio.h>
+
+void func(int* i, int* j) {
+    printf("in func, i = %d, j = %d\n", *i, *j);
+    int t;
+    t=*j;
+    *j=*i;
+    *i=t;
+    printf("in func, i = %d, j = %d\n", *i, *j);
+}
+
+int main() {
+    int n=1, m=2;
+    func(&n, &m);
+    printf("in main, n = %d, m = %d\n", n, m);
+    return 0;
+}
