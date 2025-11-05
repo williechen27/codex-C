87d88820be10ed0baa59a7b351580ae74c35531c
williechen27 <williechen27@gmail.com>
2025-11-05 22:09:48 +0800

    Update and rename Swap - Call by value to Swap - Call by value main.c

--- EX1/Swap - Call by value main.c
+++ EX1/Swap - Call by value main.c
@@ -18,3 +18,22 @@ int main() {
     return 0;
 }
 // 在 Swap func 裡操作的a和b，其實是和 main func 裡的a和b不一樣的
+
+/* show the value in memory */
+#include <stdio.h>
+
+void func(int i, int j) {
+    printf("in func, i = %d, j = %d\n", i, j);
+    int t;
+    t=j;
+    j=i;
+    i=t;
+    printf("in func, i = %d, j = %d\n", i, j);
+}
+
+int main() {
+    int i=10, j=100;
+    func(i, j);
+    printf("in main, i = %d, j = %d\n", i, j);
+    return 0;
+}
