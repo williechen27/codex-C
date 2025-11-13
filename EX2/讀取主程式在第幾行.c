5ea400758504ca07c33a47ca212d6765bafc9a8a
williechen27 <williechen27@gmail.com>
2025-11-13 13:58:33 +0800

    Update and rename 讀取主程式在第幾行 to 讀取主程式在第幾行.c

--- EX2/讀取主程式在第幾行.c
+++ EX2/讀取主程式在第幾行.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
+int main() {
     const char *input_path = "main.c";
     const char *output_path = "main.txt";
     FILE *input = fopen(input_path, "r");
     FILE *output = fopen(output_path, "w");
-
-    if (!output) {
+    
+    if(!output) {
         return 1;
     }
-
+    
     int line_number = -1;
-
-    if (input) {
+    
+    if(input) {
         char buffer[1024];
         int current_line = 1;
-
-        while (fgets(buffer, sizeof(buffer), input)) {
-            if (strstr(buffer, "int main()")) {
+        
+        while(fgets(buffer, sizeof(buffer), input)) {
+            if(strstr(buffer, "int main()")) {
                 line_number = current_line;
                 break;
             }
             current_line++;
         }
-
+        
         fclose(input);
     }
-
+    
     fprintf(output, "%d\n", line_number);
     fclose(output);
-
+    
     return 0;
 }
